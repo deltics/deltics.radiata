d5d7a815bbeffddff40a7780dec67563c573a9fd
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2020-12-31 13:29:10 +1300

    refactor: Renamed test project

---
+++ tests/test.dpr
@@ -0,0 +1,25 @@
+
+program test;
+
+  uses
+  FastMM4,
+  Deltics.Smoketest,
+  ConfigurationTests in 'ConfigurationTests.pas',
+  Deltics.Radiata.Interfaces in '..\src\Deltics.Radiata.Interfaces.pas',
+  Deltics.Radiata.LogEvent in '..\src\Deltics.Radiata.LogEvent.pas',
+  Deltics.Radiata.LogEventProperties in '..\src\Deltics.Radiata.LogEventProperties.pas',
+  Deltics.Radiata.LogEventProperty in '..\src\Deltics.Radiata.LogEventProperty.pas',
+  Deltics.Radiata.Logger.Base in '..\src\Deltics.Radiata.Logger.Base.pas',
+  Deltics.Radiata.Logger.Configuration in '..\src\Deltics.Radiata.Logger.Configuration.pas',
+  Deltics.Radiata.Logger.Null in '..\src\Deltics.Radiata.Logger.Null.pas',
+  Deltics.Radiata.Logger in '..\src\Deltics.Radiata.Logger.pas',
+  Deltics.Radiata.Logger.Sinks.Console in '..\src\Deltics.Radiata.Logger.Sinks.Console.pas',
+  Deltics.Radiata.Logger.Sinks.Debug in '..\src\Deltics.Radiata.Logger.Sinks.Debug.pas',
+  Deltics.Radiata.Logger.Sinks in '..\src\Deltics.Radiata.Logger.Sinks.pas',
+  Deltics.Radiata.Logger.Unconfigured in '..\src\Deltics.Radiata.Logger.Unconfigured.pas',
+  Deltics.Radiata.MessageTemplate in '..\src\Deltics.Radiata.MessageTemplate.pas',
+  Deltics.Radiata in '..\src\Deltics.Radiata.pas',
+  Deltics.Radiata.Utils in '..\src\Deltics.Radiata.Utils.pas';
+
+begin
+end.
