bf48aa39c865f1d61d00f9f26a70b95d1ce7cf79
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2020-12-31 17:16:58 +1300

    fix: Compatibility for Delphi XE

--- src/Deltics.Radiata.LogEventProperty.pas
+++ src/Deltics.Radiata.LogEventProperty.pas
@@ -158,7 +158,7 @@ implementation
     {$endif !NEXTGEN}
 
       vtVariant:
-      {$ifdef DELPHI2010__}
+      {$ifdef DELPHIXE__}
        if Assigned(System.VarToUStrProc) then
         begin
           System.VarToUStrProc(s, TVarData(aValue.VVariant^));
