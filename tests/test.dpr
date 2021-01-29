8ab1ca50af7a6768c9a135f3c8e0803b43aa64dc
Jolyon Direnko-Smith <jsmith@deltics.co.nz>
2021-01-30 10:59:07 +1300

    chore: Unit order

--- tests/test.dpr
+++ tests/test.dpr
@@ -4,21 +4,21 @@ program test;
   uses
   FastMM4,
   Deltics.Smoketest,
-  ConfigurationTests in 'ConfigurationTests.pas',
+  Deltics.Radiata in '..\src\Deltics.Radiata.pas',
   Deltics.Radiata.Interfaces in '..\src\Deltics.Radiata.Interfaces.pas',
   Deltics.Radiata.LogEvent in '..\src\Deltics.Radiata.LogEvent.pas',
   Deltics.Radiata.LogEventProperties in '..\src\Deltics.Radiata.LogEventProperties.pas',
   Deltics.Radiata.LogEventProperty in '..\src\Deltics.Radiata.LogEventProperty.pas',
+  Deltics.Radiata.Logger in '..\src\Deltics.Radiata.Logger.pas',
   Deltics.Radiata.Configuration in '..\src\Deltics.Radiata.Configuration.pas',
   Deltics.Radiata.Logger.Null in '..\src\Deltics.Radiata.Logger.Null.pas',
-  Deltics.Radiata.Logger in '..\src\Deltics.Radiata.Logger.pas',
   Deltics.Radiata.Sinks.Console in '..\src\Deltics.Radiata.Sinks.Console.pas',
   Deltics.Radiata.Sinks.Debug in '..\src\Deltics.Radiata.Sinks.Debug.pas',
   Deltics.Radiata.Sinks in '..\src\Deltics.Radiata.Sinks.pas',
   Deltics.Radiata.Logger.Unconfigured in '..\src\Deltics.Radiata.Logger.Unconfigured.pas',
   Deltics.Radiata.MessageTemplate in '..\src\Deltics.Radiata.MessageTemplate.pas',
-  Deltics.Radiata in '..\src\Deltics.Radiata.pas',
-  Deltics.Radiata.Utils in '..\src\Deltics.Radiata.Utils.pas';
+  Deltics.Radiata.Utils in '..\src\Deltics.Radiata.Utils.pas',
+  ConfigurationTests in 'ConfigurationTests.pas';
 
 begin
 end.
